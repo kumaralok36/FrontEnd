f17990c8ac3b5879c15b83c63add2982157e586d
Rahul Sinha <rahul.sinha1908@gmail.com>
2020-03-11 21:03:48 +0530

    Basic changes done for layouting rightly.

--- src/login-register/register/components/pages/ProviderRegisterForm.tsx
+++ src/login-register/register/components/pages/ProviderRegisterForm.tsx
@@ -29,20 +29,20 @@ export default class ProviderRegisterForm extends React.Component {
     arr = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
     arrQuestion = [];
     handleAdd = (data1) => {
-        if(data1==="" || data1===[])
-        //alert("Cannot be empty");
-        Utility.showNotification("danger","Field cannot be empty.")
-        else
-        {var mpage = this.state.page;
-        this.setState({
-            page: this.state.page !== 11 ? this.state.page + 1 : this.state.page,
-            data: data1
-        }, () => {
-            this.arr[mpage] = data1;
-            this.setState({data:this.state.data})
-            console.log(this.arr);
-        })
-    }
+        if (data1 === "" || data1 === [])
+            //alert("Cannot be empty");
+            Utility.showNotification("danger", "Field cannot be empty.")
+        else {
+            var mpage = this.state.page;
+            this.setState({
+                page: this.state.page !== 11 ? this.state.page + 1 : this.state.page,
+                data: data1
+            }, () => {
+                this.arr[mpage] = data1;
+                this.setState({ data: this.state.data })
+                console.log(this.arr);
+            })
+        }
     }
     handlePrevPage = (i) => {
         if (i > 0)
@@ -58,23 +58,23 @@ export default class ProviderRegisterForm extends React.Component {
         return (
             <div>
                 {this.arrQuestion.map((value, index) => (
-                    <li className={(arr[index]=="" || arr[index]==[] ||arr[index][0]===[])?"btn btn-info":"btn btn-success"} onClick={() => this.handleChangePage(index)} style={{ marginTop: "2%", color:"black", width: "100%" }}><b>{value.formName}</b></li>
+                    <li className={(arr[index] == "" || arr[index] == [] || arr[index][0] === []) ? "btn btn-info" : "btn btn-success"} onClick={() => this.handleChangePage(index)} style={{ marginTop: "2%", color: "black", width: "100%" }}><b>{value.formName}</b></li>
                 ))}
             </div>
         )
     }
-    handleSkipButton=()=>{
-       if(this.arrQuestion[this.state.page].canSkip)
-        this.handleChangePage(this.state.page + 1);
-       else
-       //alert("This page can't be skipped.");
-       Utility.showNotification("danger","This page can't be skipped.");
+    handleSkipButton = () => {
+        if (this.arrQuestion[this.state.page].canSkip)
+            this.handleChangePage(this.state.page + 1);
+        else
+            //alert("This page can't be skipped.");
+            Utility.showNotification("danger", "This page can't be skipped.");
     }
 
-    myCallback:(skip:boolean)=>any;
+    myCallback: (skip: boolean) => any;
 
-    callBackNav= (callback:(skip:boolean)=>any) =>{
-        this.myCallback=callback;
+    callBackNav = (callback: (skip: boolean) => any) => {
+        this.myCallback = callback;
     }
 
     getComponent = () => {
@@ -82,13 +82,13 @@ export default class ProviderRegisterForm extends React.Component {
         var question = this.arrQuestion[this.state.page];
 
         if (question.inputType === ProviderQuestionTypes.EmailField)
-            return <InputEmail handleAdd={this.handleAdd} handlePrevPage={this.handlePrevPage} label={question.heading} page={this.state.page} arr={this.arr} callbackNav={this.callBackNav}/>
+            return <InputEmail handleAdd={this.handleAdd} handlePrevPage={this.handlePrevPage} label={question.heading} page={this.state.page} arr={this.arr} callbackNav={this.callBackNav} />
 
         else if (question.inputType === ProviderQuestionTypes.Information)
             return <InputInformation handlePrevPage={this.handlePrevPage} handleAdd={this.handleAdd} label={question.heading} page={this.state.page} information={question.value} callbackNav={this.callBackNav} />
-        
+
         else if (question.inputType === ProviderQuestionTypes.PhoneField)
-            return <InputPhone handleAdd={this.handleAdd} handlePrevPage={this.handlePrevPage} label={question.heading} page={this.state.page} arr={this.arr} callbackNav={this.callBackNav} />    
+            return <InputPhone handleAdd={this.handleAdd} handlePrevPage={this.handlePrevPage} label={question.heading} page={this.state.page} arr={this.arr} callbackNav={this.callBackNav} />
 
         else if (question.inputType === ProviderQuestionTypes.TextField)
             return <InputText handleAdd={this.handleAdd} handlePrevPage={this.handlePrevPage} label={question.heading} page={this.state.page} arr={this.arr} callbackNav={this.callBackNav} />
@@ -112,11 +112,11 @@ export default class ProviderRegisterForm extends React.Component {
 
 
 
-        
+
     }
 
     render() {
-        console.log("percent comp"+9 * (this.state.page));
+        console.log("percent comp" + 9 * (this.state.page));
         return (
             <>
 
@@ -134,52 +134,45 @@ export default class ProviderRegisterForm extends React.Component {
                     </div>
                 </nav>
 
-                <div style={{ background: "#f1f1f1" }} className="sidebar" data-color="purple" data-background-color="white" data-image="assets/img/sidebar-3.jpg">
-                    <div className="logo">
-                        <a href="/" className="simple-text logo-normal">
-                            Therapy on Demand
+
+                <div className="wrapper ">
+                    {/* style={{ background: "#f1f1f1" }} */}
+                    <div  className="sidebar" data-color="purple" data-background-color="white" data-image="assets/img/sidebar-3.jpg">
+                        <div className="logo">
+                            <a href="/" className="simple-text logo-normal">
+                                Therapy on Demand
                         </a>
+                        </div>
+                        <div className="sidebar-wrapper">
+                            <ul className="nav nav-pills nav-stacked ">
+                                <div className="row-contents" style={{ padding: "6%" }}>
+                                    {this.getSidebarList(this.arr)}
+                                </div>
+                            </ul><br></br>
+                        </div>
                     </div>
-                    <div className="sidebar-wrapper">
-                        <ul className="nav">
-                            <li style={{
-                                cursor: "pointer"
-                            }} className={`nav-item ${(true) ? "active" : ""}`}>
-                                <a className="nav-link">
-                                    <i className="material-icons">dashboard</i>
-                                    <p>Hello</p>
-                                </a>
-                            </li>
-                        </ul>
-                        <ul className="nav nav-pills nav-stacked ">
-                            <div className="row-contents" style={{ padding: "6%" }}>
-                                {this.getSidebarList(this.arr)}
+
+                    <div className="main-panel">
+                        <div className="content">
+                            <div className="container-fluid">
+                                <div className="container col-sm-7">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            {this.getComponent()}
+                                        </div>
+
+                                        <div className="card-footer">
+                                            <Progress completed={9 * (this.state.page)} />
+                                        </div>
+                                    </div>
+                                </div>
+
+
                             </div>
-                        </ul><br></br>
-                    </div>
-                </div>
-
-
-                <div className="container col-sm-7" style={{ background: "white", padding: "1%", marginTop: "2%" }}>
-                    <div className="card" style={{ background: "#33a8ff", color: "white" }}>
-                        <div className="card-header">
-                            <h3><b>Futura Health Registration</b></h3>
-                            <div>Sign up to receive more information about becoming a provider in the Futura.Health ecosysteming, including Therapy on Demand</div>
-                            <div style={{ color: "red" }}>required*</div>
                         </div>
                     </div>
-                    <div className="card">
-                        {this.getComponent()}
-                    </div>
-                    <div className="card-footer">
-                        <div>
-                            <Progress completed={9 * (this.state.page)} />
-                        </div>
 
-                    </div>
                 </div>
-
-
                 <div style={{
                     position: "fixed",
                     left: 0,
@@ -189,7 +182,7 @@ export default class ProviderRegisterForm extends React.Component {
                     padding: "15px"
                 }}>
                     <div className="container">
-                        <button className="btn btn-info pull-right" onClick={()=>{
+                        <button className="btn btn-info pull-right" onClick={() => {
                             this.myCallback(false);
                         }}>Submit</button>
                         <button className="btn btn-btn-default pull-right" onClick={this.handleSkipButton}>Skip</button>
